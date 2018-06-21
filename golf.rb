c29795382ccb21112d19433f6956ee9e2b650dfa
gretzky <sfed617@gmail.com>
2018-06-21 16:54:49 -0400

    update golf

--- golf.rb
+++ golf.rb
@@ -1,16 +1,16 @@
-class Golf.sh < Formula
-    desc "The faster way to init a project"
-    homepage "https://github.com/gretzky/golf.sh"
-    url "https://github.com/gretzky/golf.sh/archive/v0.1.1.zip"
-    sha256 ""
-    head "https://github.com/gretzky/golf.sh.git"
+class Golf < Formula
+  desc "the faster way to init a project"
+  homepage ""
+  url "https://github.com/gretzky/golf/archive/v0.1.1.tar.gz"
+  sha256 "a431b6fa61d49c682d9c12b08490a3eff071f1cbaaed97eee81c48bc4db6844a"
+  depends_on "dialog" => :build
+  depends_on "jq"
 
-    def install
-        ENV.03
-        (var/"log/golf.sh").mkpath
-        system "make", "install"
-        bin.install "#{buildpath}/bin/golf.sh"
-    end
+  def install
+    system "make", "install"
+  end
 
-    plist_options ":man" => "golf.sh"
+  test do
+    assert_match "golf.sh #{version}", shell_output("#{bin}/golf.sh --version")
+  end
 end
